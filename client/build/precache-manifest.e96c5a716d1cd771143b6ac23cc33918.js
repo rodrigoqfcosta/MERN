3e0724000a342c357ddaf6fa801ea4c416c79c2c
Rodrigo Querino <rodrigoqfcosta@gmail.com>
2020-12-03 12:21:45 -0300

    primeiro commit

---
+++ client/build/precache-manifest.e96c5a716d1cd771143b6ac23cc33918.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a2e6dfce4dbf968771744951379b26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b23e671ead38894e9de",
+    "url": "/static/css/2.b0bea79c.chunk.css"
+  },
+  {
+    "revision": "6b23e671ead38894e9de",
+    "url": "/static/js/2.0b73da2b.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.0b73da2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6bca8357a280cfbb91e",
+    "url": "/static/js/main.4d12a224.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
